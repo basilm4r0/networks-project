01f01a9f1b54f3065779aa2549f047b73a4a7fd6
Basil Mari <basilm4r0@gmail.com>
2022-05-13 15:27:36 +0300

    Add redirection to google.com, cnn.com, and birzeit.edu

--- server.py
+++ server.py
@@ -7,36 +7,50 @@ curpath = "/home/basilmari/Desktop/ENCS3320/project_1/"
 class Serv(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            header = 'HTTP/1.1 200 OK\n'
-            if self.path == '/':
-                self.path = '/main_en.html'
-                mimetype = 'text/html'
-            elif self.path == '/en':
-                self.path = '/main_en.html'
-                mimetype = 'text/html'
-            elif self.path == '/ar':
-                self.path = '/main_ar.html'
-                mimetype = 'text/html'
-            elif self.path == '/index.html':
-                self.path = '/index.html'
-                mimetype = 'text/html'
-            elif self.path == '/main.css':
-                self.path = '/main.css'
-                mimetype = 'text/css'
-            elif self.path == '/bzu.png':
-                self.path = '/bzu.png'
-                mimetype = 'image/png'
-            elif self.path == '/net1.jpg':
-                self.path = '/net1.jpg'
-                mimetype = 'image/jpg'
+            if self.path == '/go' or self.path == '/cn' or self.path == '/bzu':
+                if self.path == '/go':
+                    self.send_response(307)
+                    self.send_header('Location', 'https://www.google.com')
+                    self.end_headers()
+                elif self.path == '/cn':
+                    self.send_response(307)
+                    self.send_header('Location', 'https://www.edition.cnn.com')
+                    self.end_headers()
+                elif self.path == '/bzu':
+                    self.send_response(307)
+                    self.send_header('Location', 'https://www.birzeit.edu')
+                    self.end_headers()
+                self.wfile.write('\n'.encode('utf-8'))
             else:
-                self.path = 'wrong.html'
-                mimetype = 'text/html'
-            file_to_open = readfile(self.path)
-            self.send_response(200)
-            self.send_header('Content-type', mimetype)
-            self.end_headers()
-            self.wfile.write(file_to_open)
+                if self.path == '/':
+                    self.path = '/main_en.html'
+                    mimetype = 'text/html'
+                elif self.path == '/en':
+                    self.path = '/main_en.html'
+                    mimetype = 'text/html'
+                elif self.path == '/ar':
+                    self.path = '/main_ar.html'
+                    mimetype = 'text/html'
+                elif self.path == '/index.html':
+                    self.path = '/index.html'
+                    mimetype = 'text/html'
+                elif self.path == '/main.css':
+                    self.path = '/main.css'
+                    mimetype = 'text/css'
+                elif self.path == '/bzu.png':
+                    self.path = '/bzu.png'
+                    mimetype = 'image/png'
+                elif self.path == '/net1.jpg':
+                    self.path = '/net1.jpg'
+                    mimetype = 'image/jpg'
+                else:
+                    self.path = 'wrong.html'
+                    mimetype = 'text/html'
+                file_to_open = readfile(self.path)
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(file_to_open)
         except IOError:
             self.send_error(404, "File not found %s" % self.path)
 
