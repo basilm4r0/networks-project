852383252e0ef332673d9b5961cc4c9a0b677eb6
Basil Mari <basilm4r0@gmail.com>
2022-05-15 16:52:23 +0300

    rewrite server without libraries

--- server2.py
+++ server2.py
@@ -1,83 +1,10 @@
-import http.server
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
 
 port = 9000
+HEADER = 1024
 curpath = "/home/basilmari/Desktop/ENCS3320/project_1/" # path to root directory of server
-
-class Serv(BaseHTTPRequestHandler):     # define request handler
-    def do_GET(self):       # define response method
-        try:
-            if self.path == '/go' or self.path == '/cn' or self.path == '/bzu':     # determine requested page
-                if self.path == '/go':
-                    self.send_response(307)     # redirection status code
-                    self.send_header('Location', 'https://www.google.com')  # site to be redirected to
-                    self.end_headers()
-                elif self.path == '/cn':
-                    self.send_response(307)
-                    self.send_header('Location', 'https://www.edition.cnn.com')
-                    self.end_headers()
-                elif self.path == '/bzu':
-                    self.send_response(307)
-                    self.send_header('Location', 'https://www.birzeit.edu')
-                    self.end_headers()
-                self.wfile.write('\n'.encode('utf-8'))
-            if (self.path == '/' or self.path == '/en' or self.path == '/ar' or self.path == '/index.html' or self.path == '/main.css' or self.path
-            == '/bzu.png' or self.path == '/net1.jpg' or self.path == '/networking2.jpg'):
-                if self.path == '/':
-                    self.path = '/main_en.html'
-                    mimetype = 'text/html'      # set content type for header
-                elif self.path == '/en':
-                    self.path = '/main_en.html'
-                    mimetype = 'text/html'
-                elif self.path == '/ar':
-                    self.path = '/main_ar.html'
-                    mimetype = 'text/html'
-                elif self.path == '/index.html':
-                    self.path = '/index.html'
-                    mimetype = 'text/html'
-                elif self.path == '/main.css':
-                    self.path = '/main.css'
-                    mimetype = 'text/css'
-                elif self.path == '/bzu.png':
-                    self.path = '/bzu.png'
-                    mimetype = 'image/png'
-                elif self.path == '/net1.jpg':
-                    self.path = '/net1.jpg'
-                    mimetype = 'image/jpg'
-                elif self.path == '/networking2.jpg':
-                    self.path = '/networking2.jpg'
-                    mimetype = 'image/jpg'
-                file_to_open = readfile(self.path)
-                self.send_response(200)     # OK status code
-                self.send_header('Content-type', mimetype)  # set header
-                self.end_headers()
-                self.wfile.write(file_to_open)  # respond with requested page
-            else:           # page not found
-                file_to_open = '''<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title> Error </title>
-  </head>
-  <body>
-    <h1> HTTP/1.1 404 Not Found </h1>
-    <h2 style="color:red;"> The file is not found </h2>
-    <div>
-      <b> Ahmad Abu Masood - 1192647</b> <br/>
-      <b> Basil Mari - 1191027</b> <br/>
-      <b> Mohammad Nafee - 1173027</b>
-    </div>
-    <p> The IP number of the Client is %s</p>
-    <p> The port number of the Client is %s</p>
-  </body>
-</html>''' % (self.address_string(), port)      # Page not found html page
-                mimetype = 'text/html'
-                self.send_response(404)     # page not found status code
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                self.wfile.write(file_to_open.encode('utf-8'))      # respond with page not found page
-
-        except IOError:
-            self.send_error(404, "File not found %s" % self.path)
+server_address = socket.gethostbyname(socket.gethostname())
+address = (server_address, port)
 
 def readfile(path):     # reads files as binary
     file = open(curpath + path, 'rb')
@@ -87,13 +14,97 @@ def readfile(path):     # reads files as binary
 
 def main():     # main body. instantiates server and handles keyboard interrupt
     try:
-        server = HTTPServer(('', port), Serv)
-        print("Web server running on  port %s" % port)
-        server.serve_forever()
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(address)
+        server.listen(1)
+        print("Web server running on  port %s, IP address %s" % (port, server_address))
+        while True:
+            connection, client = server.accept()
+            print("client IP address: %s\nclient port: %s" % (client[0], client[1]))
+            request = connection.recv(HEADER).decode('utf-8')
+            message = request.split(' ')
+            method = message[0]
+            filepath = message[1]
+            print("Request:\t%s %s" % (method, filepath))
+            ok_header = 'HTTP/1.1 200 OK\n'
+            redirect_header = 'HTTP/1.1 307 Temporary Redirect\n'
+            try:
+                if filepath == '/go' or filepath == '/cn' or filepath == '/bzu':     # determine requested page
+                    if filepath == '/go':
+                        header = redirect_header
+                        location = 'https://www.google.com'  # site to be redirected to
+                    elif filepath == '/cn':
+                        header = redirect_header
+                        location = 'https://www.edition.cnn.com'  # site to be redirected to
+                    elif filepath == '/bzu':
+                        header = redirect_header
+                        location = 'https://www.birzeit.edu'  # site to be redirected to
+                    connection.send(redirect_header)
+                    connection.send(f'Location : {location}\n\n'.encode('utf-8'))
+                elif (filepath == '/' or filepath == '/en' or filepath == '/ar' or filepath == '/index.html' or filepath == '/main.css' or filepath
+                == '/bzu.png' or filepath == '/net1.jpg' or filepath == '/networking2.jpg'):
+                    if filepath == '/':
+                        filepath = '/main_en.html'
+                        mimetype = 'text/html'      # set content type for header
+                    elif filepath == '/en':
+                        filepath = '/main_en.html'
+                        mimetype = 'text/html'
+                    elif filepath == '/ar':
+                        filepath = '/main_ar.html'
+                        mimetype = 'text/html'
+                    elif filepath == '/index.html':
+                        filepath = '/index.html'
+                        mimetype = 'text/html'
+                    elif filepath == '/main.css':
+                        filepath = '/main.css'
+                        mimetype = 'text/css'
+                    elif filepath == '/bzu.png':
+                        filepath = '/bzu.png'
+                        mimetype = 'image/png'
+                    elif filepath == '/net1.jpg':
+                        filepath = '/net1.jpg'
+                        mimetype = 'image/jpg'
+                    elif filepath == '/networking2.jpg':
+                        filepath = '/networking2.jpg'
+                        mimetype = 'image/jpg'
+                    file_to_open = readfile(filepath)
+                    header = ok_header     # OK status code
+                    header += 'Content-Type: ' + mimetype + '\n\n'  # set header
+                else:           # page not found
+                    file_to_open = '''<html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title> Error </title>
+      </head>
+      <body>
+        <h1> HTTP/1.1 404 Not Found </h1>
+        <h2 style="color:red;"> The file is not found </h2>
+        <div>
+          <b> Ahmad Abu Masood - 1192647</b> <br/>
+          <b> Basil Mari - 1191027</b> <br/>
+          <b> Mohammad Nafee - 1173027</b>
+        </div>
+        <p> The IP address of the Client is %s</p>
+        <p> The port number of the Client is %s</p>
+        <p> The IP address of the server is %s</p>
+        <p> The port number of the server is %s</p>
+      </body>
+    </html>''' % (client[0], client[1], address[0], address[1])      # Page not found html page
+                    mimetype = 'text/html'
+                    header = 'HTTP/1.1 404 Not Found\n\n'    # page not found status code
+                    header += 'Content-Type: ' + mimetype + '\n\n'  # set header
+                print(header)
+                response = str(header) + str(file_to_open)
+                response = response.encode('utf-8')
+                connection.sendall(response)
+                connection.close()
 
+            except IOError:
+                connection.close()
     except KeyboardInterrupt:
         print("^C pressed, stopping web server...")
-        server.socket.close()
+        quit()
 
 if __name__ == '__main__':
     main()
